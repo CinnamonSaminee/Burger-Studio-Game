03d7ee706702bfdbb70f921280a8085c55573eb9
Samin Saberi <saminsaberi77@hotmail.com>
2017-07-06 13:50:39 -0400

    Change Meat to extend Ingredient

--- Meat.java
+++ Meat.java
@@ -17,20 +17,19 @@ import java.awt.Rectangle;
 import javax.swing.ImageIcon;
 
 //Class representing the patties
-public class Meat
-{
+public class Meat extends Ingredient {
 
     private String craft = "Meat.png";
 
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    private boolean visible;
-    //Booleans determine if its falling or stacked
-    private boolean fall = true;
-    private boolean stacked = false;
-    private Image image;
+//    private int x;
+//    private int y;
+//    private int width;
+//    private int height;
+//    private boolean visible;
+//    //Booleans determine if its falling or stacked
+//    private boolean fall = true;
+//    private boolean stacked = false;
+//    private Image image;
 
 
 
